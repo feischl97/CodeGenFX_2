127792ce2f67cac438442666b6ac5e55e884add2
Felix Hillebrand <97hilfel@gmail.com>
2017-05-02 13:05:25 +0200

    Refactored sample.fxml to MainWindow.fxml cleared Controller.java

--- src/x19de/Main.java
+++ src/x19de/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         
         primaryStage.setTitle("CodeGenFXTWO");
         primaryStage.setScene(new Scene(root, 600, 300));
